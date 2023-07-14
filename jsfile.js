3f99616f801dd9f002129542d535db95f6d9036b
Omitul <omitul1010@gmail.com>
2023-07-14 11:34:34 -0700

    opened a css file ,designed button,opened a js file make the button work, linked css,js to html file

--- jsfile.js
+++ jsfile.js
@@ -1,1 +1,7 @@
-alert("asche");
+///alert("asche"); eta dile page open korlei ei msg asbe, apatoto comment kore rakhlam
+(function(){
+  const button = document.getElementById('btn');
+  button.addEventListener('click',function() {
+     alert('succesfully clicked');   
+  });
+})();
